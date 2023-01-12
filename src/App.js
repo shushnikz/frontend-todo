184cc8a4d13d45b7e39f607b9acab8b3253cfd67
shushnikz <sushmithadevadiga997@gmail.com>
2023-01-12 20:06:12 +0530

    added background

--- src/App.js
+++ src/App.js
@@ -59,58 +59,60 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <div className="row">
-        <pre>{JSON.stringify(todo)}</pre>
-        <form className="col s12" onSubmit={onSubmitHandler}>
-          <div className="row">
-            <div className="input-field col s6">
-              <i className="material-icons prefix">title</i>
-              <input id="icon_prefix" type="text" className="validate"
-                value={todo.title}
-                onChange={(e) => setTodo({ ...todo, title: e.target.value })}
-              ></input>
-              <label htmlFor="icon_prefix">Title</label>
+    <div className='app'>
+      <div className="container">
+        <div className="row">
+          <pre>{JSON.stringify(todo)}</pre>
+          <form className="col s12" onSubmit={onSubmitHandler}>
+            <div className="row">
+              <div className="input-field col s6">
+                <i className="material-icons prefix">title</i>
+                <input id="icon_prefix" type="text" className="validate"
+                  value={todo.title}
+                  onChange={(e) => setTodo({ ...todo, title: e.target.value })}
+                ></input>
+                <label htmlFor="icon_prefix">Title</label>
+              </div>
+              <div className="input-field col s6">
+                <i className="material-icons prefix">description</i>
+                <input id="description" type="tel" className="validate"
+                  value={todo.content}
+                  onChange={(e) => setTodo({ ...todo, content: e.target.value })}
+                ></input>
+                <label htmlFor="description">Content</label>
+              </div>
             </div>
-            <div className="input-field col s6">
-              <i className="material-icons prefix">description</i>
-              <input id="description" type="tel" className="validate"
-                value={todo.content}
-                onChange={(e) => setTodo({ ...todo, content: e.target.value })}
-              ></input>
-              <label htmlFor="description">Content</label>
+            <div className='row right-align'>
+              <button className='waves-effect waves-light btn'>
+                Submit
+              </button>
             </div>
-          </div>
-          <div className='row right-align'>
-            <button className='waves-effect waves-light btn'>
-              Submit
-            </button>
-          </div>
-        </form>
-        {
-          !todos ? <Preloader /> : todos.length > 0 ?
-            <ul className="collection">
-              {todos.map((todo) => (
-                <li key={todo._id}
-                  onClick={() => setCurrentId(todo._id)}
-                  className="collection-item">
-                  <div>
-                    <h5>{todo.title}</h5>
-                    <p>{todo.content}
-                      <a href="#!"
-                        onClick={() => removeTodo(todo._id)}
-                        className="secondary-content">
-                        <i className="material-icons">delete</i>
-                      </a>
-                    </p>
-                  </div>
-                </li>
-              ))}
+          </form>
+          {
+            !todos ? <Preloader /> : todos.length > 0 ?
+              <ul className="collection">
+                {todos.map((todo) => (
+                  <li key={todo._id}
+                    onClick={() => setCurrentId(todo._id)}
+                    className="collection-item">
+                    <div>
+                      <h5 className='blue-text text-darken-4'>{todo.title}</h5>
+                      <p className='black-text text-darken-2'>{todo.content}
+                        <a href="#!"
+                          onClick={() => removeTodo(todo._id)}
+                          className="secondary-content">
+                          <i className="material-icons">delete</i>
+                        </a>
+                      </p>
+                    </div>
+                  </li>
+                ))}
 
-            </ul> : <div><h5>Nothing To Do</h5></div>
-        }
+              </ul> : <div><h5>Nothing To Do</h5></div>
+          }
 
 
+        </div>
       </div>
     </div>
   );
