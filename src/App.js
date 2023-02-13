9fd738b7bb7e481d77c162ad75567d3291a110d3
shushnikz <sushmithadevadiga997@gmail.com>
2023-02-13 17:13:08 +0530

    addedd stike

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Preloader from './components/Preloader';
 import { createTodo, deleteTodo, readTodos, updateTodo } from './functions';
+import Checkbox from '@mui/material/Checkbox';
+import EditIcon from '@mui/icons-material/Edit';
 
 function App() {
 
   const [todo, setTodo] = useState({ title: '', content: '' })
   const [todos, setTodos] = useState(null)
   const [currentId, setCurrentId] = useState(0)
+  const [update, setupdate] = useState(false)
+
+
 
   useEffect(() => {
     let currentTodo = currentId !== 0 ? todos.find(todo => todo._id === currentId) :
@@ -16,14 +21,18 @@ function App() {
   }, [currentId])
 
   const fetchData = async () => {
-    const result = await readTodos();
+    let result = await readTodos();
+    result = result.map((e) => {
+      return { ...e, check: e.check ?? false }
+    })
+    console.log(result)
     setTodos(result)
   }
 
   useEffect(() => {
 
     fetchData()
-  }, [currentId])
+  }, [])
 
   const clear = () => {
     setCurrentId(0);
@@ -47,7 +56,8 @@ function App() {
       setTodos([...todos, result])
       clear();
     } else {
-      await updateTodo(currentId, todo)
+      await updateTodo(currentId, todo).then(() => fetchData())
+      setupdate(false)
       clear();
     }
 
@@ -61,11 +71,22 @@ function App() {
     fetchData()
   }
 
+  const striketodo = (index) => {
+    console.log(todos);
+    const newTodos = [...todos];
+    newTodos[index].check = !newTodos[index].check;
+    setTodos(newTodos);
+  };
+
+  console.log(update, "shush")
+
   return (
     <div className='app'>
       <div className="container">
         <div className="row">
-          <pre>{JSON.stringify(todo)}</pre>
+          <div className='content'>
+            <pre>{JSON.stringify(todo)}</pre>
+          </div>
           <form className="col s12" onSubmit={onSubmitHandler}>
             <div className="row">
               <div className="input-field col s6">
@@ -87,28 +108,60 @@ function App() {
             </div>
             <div className='row right-align'>
               <button className='waves-effect waves-light btn'>
-                Add
+                {update ? "Update" : "Add"}
               </button>
             </div>
           </form>
           {
             !todos ? <Preloader /> : todos.length > 0 ?
               <ul className="collection">
-                {todos.map((todo) => (
+                {todos.map((todo, i) => (
                   <li key={todo._id}
-                    onClick={() => setCurrentId(todo._id)}
+
                     className="collection-item">
+
                     <div>
-                      <h5 className='blue-text text-darken-4'>{todo.title}</h5>
+
+                      <h5 className='blue-text text-darken-4'>
+
+                        <Checkbox
+                          onClick={() => {
+
+                            striketodo(i)
+
+                          }
+                          }
+                          checked={todo.check}
+                        />
+
+                        <label style={{ textDecoration: todo.check ? "line-through" : "none", fontSize: "20px", fontWeight: "bold" }}>
+                          {todo.title}
+                        </label>
+                      </h5>
+
                       <p className='black-text text-darken-2'>{todo.content}
                         <a href="#!"
                           onClick={() => removeTodo(todo._id)}
                           className="secondary-content">
                           <i className="material-icons">delete</i>
+
+                        </a>
+                        <a href="#!"
+                          onClick={() => {
+                            setupdate(true)
+                            setCurrentId(todo._id)
+                          }}
+                          className="secondary-content">
+
+                          <EditIcon color="secondary"
+                            style={{ margin: "5px" }} />
                         </a>
                       </p>
                     </div>
                   </li>
+
+
+
                 ))}
 
               </ul> : <div><h5>Nothing To Do</h5></div>
